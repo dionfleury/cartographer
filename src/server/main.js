70c201bca4f1fdf09560ae2f43dc508168dad57e
Dion Fleury <76707215+dionfleury@users.noreply.github.com>
2024-10-28 11:07:27 +0100

    Modified express server to add https service in production

--- src/server/main.js
+++ src/server/main.js
@@ -1,6 +1,10 @@
 import express from "express"
 import ViteExpress from "vite-express"
 import cors from "cors"
+import https from "https"
+import fs from "fs"
+import path from "path"
+
 
 ViteExpress.config( { mode: process.env.NODE_ENV } )
 
@@ -8,12 +12,21 @@ const app = express()
 
 app.use( cors() )
 
+app.get( "/hello", ( req, res ) => { res.send( "Hello Vite + React!" ) } )
 
-app.get( "/hello", ( req, res ) =>
+if ( process.env.NODE_ENV === "production" )
 {
-  res.send( "Hello Vite + React!" )
-} )
+  const certFile = process.env.CERT_FILE
+  const keyFile = process.env.CERTKEY_FILE
 
-ViteExpress.listen( app, 3000, () =>
-  console.log( "Server is listening on port 3000..." ),
-)
+  const options = {
+    key: fs.readFileSync( path.resolve( keyFile ) ),
+    cert: fs.readFileSync( path.resolve( certFile ) ),
+  }
+
+  const server = https.createServer( options, app )
+
+  server.listen( 3443, () => console.log( "Server is listening on port 3000..." ) )
+}
+
+ViteExpress.listen( app, 3000, () => console.log( "Server is listening on port 3000..." ) )
